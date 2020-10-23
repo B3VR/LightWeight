9d2a23c7ac9ac823b8abfcb170e8444ed52c1d88
Krystian Miszta <krysmis479@student.polsl.pl>
2020-10-23 19:24:19 +0200

    widok profilu

--- app/src/main/java/com/example/lightweight/EditProfileFragment.kt
+++ app/src/main/java/com/example/lightweight/EditProfileFragment.kt
@@ -1,13 +1,17 @@
 package com.example.lightweight
 
+import User
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.fragment_edit_profile.*
 
 
-class EditProfileFragment : Fragment() {
+class EditProfileFragment : Fragment(), View.OnClickListener {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,5 +19,46 @@ class EditProfileFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_edit_profile, container, false)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+
+    private fun editProfile(){
+        var name: String = view?.findViewById<TextView>(R.id.editNameTxt).toString()
+        var weight: Double = view?.findViewById<TextView>(R.id.editWeightTxt).toString().toDouble()
+        var height: Double = view?.findViewById<TextView>(R.id.editHeightTxt).toString().toDouble()
+
+        var ref = FirebaseDatabase.getInstance().getReference("User")
+        var user: User = User(name, weight, height)
+        val userId = ref.push().key
+
+        ref.child(userId).setValue(user)
+
+
+
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.editProfileBtn -> {
+                if(editNameTxt.text.isEmpty()){
+                    editNameTxt.error = "Wymagana nazwa użytkownika"
+                    return
+                }else if (editHeightTxt.text.isEmpty()){
+                    editHeightTxt.error = "Wymagany wzrost"
+                    return
+                }else if (editWeightTxt.text.isEmpty()){
+                    editHeightTxt.error = "Wymagana waga"
+                    return
+                }else
+
+                    editProfile()
+            }
+        }
+
     }
 }
--- app/src/main/java/com/example/lightweight/ProfileFragment.kt
+++ app/src/main/java/com/example/lightweight/ProfileFragment.kt
@@ -5,11 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import kotlinx.android.synthetic.*
 
-class ProfileFragment : Fragment() {
+class ProfileFragment : Fragment(), View.OnClickListener {
 
+    var navControler: NavController? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,6 +26,9 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        navControler = Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.editProfileBtn2).setOnClickListener(this)
+
         var bmi = view.findViewById<TextView>(R.id.bmiView)
         bmi.text = getBMI(85.0,1.5).toShort().toString()
     }
@@ -31,6 +38,12 @@ class ProfileFragment : Fragment() {
         bmi = weight/ (height*height)
 
         return bmi
+    }
+
+    override fun onClick(v: View?){
+        when(v!!.id){
+            R.id.editProfileBtn2 -> navControler!!.navigate(R.id.action_profileFragment_to_editProfileFragment)
+        }
     }
 }
 
